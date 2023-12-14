ecf5c9c3ea766a100bcbbe911c0d7788e7beb942
Santiago Fernandez <santifer710@hotmail.com>
2023-12-14 11:01:13 -0300

    cambios para que login se pase a llamar index

--- Utiles.js
+++ Utiles.js
@@ -9,12 +9,12 @@ export const obtenerValorInput = (idInput) => document.getElementById(idInput).v
 
 export const validarSesion = () => {
   const sesionActiva = sessionStorage.getItem('sesionActiva') === 'TRUE';
-  const estaEnLogin = document.location.pathname.includes('login.html');
+  const estaEnLogin = document.location.pathname.includes('index.html');
 
   if (!sesionActiva) {
 
     if (!estaEnLogin) {
-      document.location.replace('login.html');
+      document.location.replace('index.html');
     }
   } else if (estaEnLogin) {
 
